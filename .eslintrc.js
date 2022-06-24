cf4f73f9eec53815582b1996390f53e63dc83fcf
vigor-13 <vigor13.dev@gmail.com>
2022-06-24 18:26:28 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.eslint.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
