7d1cd08c117da9593742d0c4df0def4da6e127ef
vigor-13 <vigor13.dev@gmail.com>
2022-06-27 15:29:40 +0900

    build: add ts-node

--- .pnp.cjs
+++ .pnp.cjs
@@ -59,6 +59,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["prettier", "npm:2.6.2"],\
             ["rimraf", "npm:3.0.2"],\
             ["ts-jest", "virtual:3d6386df8a67a1a43258bc939f2a6e56c2fd6baedb2eac09512dbbc449388205d7de34aa62492bc9c9c47c9fab4fc43f8e50ba217ccf334def8c19d2976f8a86#npm:28.0.4"],\
+            ["ts-node", "virtual:3d6386df8a67a1a43258bc939f2a6e56c2fd6baedb2eac09512dbbc449388205d7de34aa62492bc9c9c47c9fab4fc43f8e50ba217ccf334def8c19d2976f8a86#npm:10.8.1"],\
             ["typescript", "patch:typescript@npm%3A4.7.3#~builtin<compat/typescript>::version=4.7.3&hash=7ad353"]\
           ],\
           "linkType": "SOFT"\
@@ -1126,6 +1127,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["prettier", "npm:2.6.2"],\
             ["rimraf", "npm:3.0.2"],\
             ["ts-jest", "virtual:3d6386df8a67a1a43258bc939f2a6e56c2fd6baedb2eac09512dbbc449388205d7de34aa62492bc9c9c47c9fab4fc43f8e50ba217ccf334def8c19d2976f8a86#npm:28.0.4"],\
+            ["ts-node", "virtual:3d6386df8a67a1a43258bc939f2a6e56c2fd6baedb2eac09512dbbc449388205d7de34aa62492bc9c9c47c9fab4fc43f8e50ba217ccf334def8c19d2976f8a86#npm:10.8.1"],\
             ["typescript", "patch:typescript@npm%3A4.7.3#~builtin<compat/typescript>::version=4.7.3&hash=7ad353"]\
           ],\
           "linkType": "SOFT"\
@@ -9388,6 +9390,42 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["ts-node", "npm:10.8.1"]\
           ],\
           "linkType": "SOFT"\
+        }],\
+        ["virtual:3d6386df8a67a1a43258bc939f2a6e56c2fd6baedb2eac09512dbbc449388205d7de34aa62492bc9c9c47c9fab4fc43f8e50ba217ccf334def8c19d2976f8a86#npm:10.8.1", {\
+          "packageLocation": "./.yarn/__virtual__/ts-node-virtual-7262b80366/0/cache/ts-node-npm-10.8.1-24280b0982-7d1aa7aa3a.zip/node_modules/ts-node/",\
+          "packageDependencies": [\
+            ["ts-node", "virtual:3d6386df8a67a1a43258bc939f2a6e56c2fd6baedb2eac09512dbbc449388205d7de34aa62492bc9c9c47c9fab4fc43f8e50ba217ccf334def8c19d2976f8a86#npm:10.8.1"],\
+            ["@cspotcode/source-map-support", "npm:0.8.1"],\
+            ["@swc/core", null],\
+            ["@swc/wasm", null],\
+            ["@tsconfig/node10", "npm:1.0.8"],\
+            ["@tsconfig/node12", "npm:1.0.9"],\
+            ["@tsconfig/node14", "npm:1.0.1"],\
+            ["@tsconfig/node16", "npm:1.0.2"],\
+            ["@types/node", "npm:18.0.0"],\
+            ["@types/swc__core", null],\
+            ["@types/swc__wasm", null],\
+            ["@types/typescript", null],\
+            ["acorn", "npm:8.7.1"],\
+            ["acorn-walk", "npm:8.2.0"],\
+            ["arg", "npm:4.1.3"],\
+            ["create-require", "npm:1.1.1"],\
+            ["diff", "npm:4.0.2"],\
+            ["make-error", "npm:1.3.6"],\
+            ["typescript", "patch:typescript@npm%3A4.7.3#~builtin<compat/typescript>::version=4.7.3&hash=7ad353"],\
+            ["v8-compile-cache-lib", "npm:3.0.1"],\
+            ["yn", "npm:3.1.1"]\
+          ],\
+          "packagePeers": [\
+            "@swc/core",\
+            "@swc/wasm",\
+            "@types/node",\
+            "@types/swc__core",\
+            "@types/swc__wasm",\
+            "@types/typescript",\
+            "typescript"\
+          ],\
+          "linkType": "HARD"\
         }],\
         ["virtual:9e9d2b94dac68914cfc6173c41e92914f6d1fe30b9ab6e163c2043744960a73d771c78f37827a8773b7adeb75f4b67f907cd8c18197a7807ce979a4aea3b2602#npm:10.8.1", {\
           "packageLocation": "./.yarn/__virtual__/ts-node-virtual-eeffb9cf42/0/cache/ts-node-npm-10.8.1-24280b0982-7d1aa7aa3a.zip/node_modules/ts-node/",\
